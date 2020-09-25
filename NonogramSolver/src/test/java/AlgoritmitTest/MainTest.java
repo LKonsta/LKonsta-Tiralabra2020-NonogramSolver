6afe4aebf31446a80fa3680a0f47bcf790eff472
LKonsta <komppa.lehto@gmail.com>
2020-09-25 08:45:14 +0300

    puhdistus ja pom filu kuntoon jotta testejäkin voi tehdä commit

--- NonogramSolver/src/test/java/AlgoritmitTest/MainTest.java
+++ NonogramSolver/src/test/java/AlgoritmitTest/MainTest.java
@@ -1,22 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package AlgoritmitTest;
 
+
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
-/**
- *
- * @author khlehto
- */
+
+
 public class MainTest {
     
     @Test
     public void SimppeliTest() {
-        Assert.assertEquals();
+        Assert.assertEquals(1, 1);
     }
     
 }
